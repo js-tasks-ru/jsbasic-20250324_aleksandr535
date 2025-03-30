a66e5ec8175f0e819f86c8ad24288f71e82917d9
FlyDream666 <sanek1997_7@mail.ru>
2025-03-30 20:30:15 +0300

    search spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
-  // ваш код...
+  const lowerStr = str.toLowerCase();
+    const spamWords = ['1xbet', 'xxx'];
+
+    for (const word of spamWords) {
+        if (lowerStr.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
 }
