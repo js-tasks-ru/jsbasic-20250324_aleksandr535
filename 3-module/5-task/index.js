827e84a777830d76cb72742370c200afe5695d21
FlyDream666 <sanek1997_7@mail.ru>
2025-04-06 17:45:05 +0300

    function and array

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+
+  let numbers = arr
+        .map(part => {
+            const parsed = parseFloat(part);
+            return isFinite(parsed) ? parsed : null;
+        })
+        .filter(Boolean);
+
+    let minValue = Math.min(...numbers);
+    let maxValue = Math.max(...numbers);
+
+    return { min: minValue, max: maxValue };
 }
