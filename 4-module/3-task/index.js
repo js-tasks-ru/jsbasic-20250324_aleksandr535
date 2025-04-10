2c6db4e3697672ea2236ccfac3428644616c7ff9
FlyDream666 <sanek1997_7@mail.ru>
2025-04-10 21:45:20 +0300

    DOM

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+
+  rows.forEach(row => {
+      const statusCell = row.querySelector('td[data-available]');
+
+      if (statusCell) {
+          if (statusCell.dataset.available === 'true') {
+              row.classList.add('available');
+          } else if (statusCell.dataset.available === 'false') {
+              row.classList.add('unavailable');
+          }
+      } else {
+          row.setAttribute('hidden', '');
+      }
+
+      const genderCell = row.querySelector('td:nth-child(3)');
+
+      if (genderCell.textContent === 'm') {
+          row.classList.add('male');
+      } else if (genderCell.textContent === 'f') {
+          row.classList.add('female');
+      }
+
+      const ageCell = row.querySelector('td:nth-child(2)');
+
+if (parseInt(ageCell.textContent) < 18) {
+    row.style.textDecoration = 'line-through';
+}
+  });
 }
